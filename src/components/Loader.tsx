490e3ae40ed1109958355040fa4a479e9023b2d5
Ayushi <ayushisaxena24111999@gmail.com>
2025-05-05 23:32:45 +0530

    Added 3d computer

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,15 @@
-const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+import { FC } from 'react';
+import { Html, useProgress } from '@react-three/drei';
 
-export default Loader
+const CanvasLoader: FC = () => {
+  const { progress } = useProgress();
+  return (
+    <Html center>
+      <div className="text-blue-600 text-center">
+        <p>Loading 3D Model... {progress.toFixed(0)}%</p>
+      </div>
+    </Html>
+  );
+};
+
+export default CanvasLoader;
