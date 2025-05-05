947f6402a9745b7e1d72aef1c9b213dcf5f08349
Ayushi <ayushisaxena24111999@gmail.com>
2025-05-05 22:45:22 +0530

    Added Hero component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,15 @@
-import { Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { logo, menu, close } from "../assets";
 import { useState } from "react";
 import { navLinks } from "../constants";
-// const {navLinks} from '../constants'
 
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -24,8 +22,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Ayushi&nbsp;
-            <span className="sm:block hidden">Saxena</span>
+            My&nbsp;
+            <span className="sm:block hidden">Portfolio</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex:grow gap-10">
