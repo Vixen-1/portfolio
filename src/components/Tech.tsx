11e4c491b27caa8a69a11a56d7360157a2348de3
Ayushi <ayushisaxena24111999@gmail.com>
2025-05-06 11:35:39 +0530

    added technologies

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,7 +1,18 @@
-const Tech = () => {
+import { FC } from "react"
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+
+const Tech: FC = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology, index)=>(
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "tech")
